d2be29195124875f2fa27f439fed41bd0d8baa89
cengizVenes <136815474+cengizVenes@users.noreply.github.com>
2023-06-19 00:47:27 +0300

    stage-bootstrapp

---
+++ Stage-Proje/Stage-Proje/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stage_Proje.Models;
+using System.Diagnostics;
+
+namespace Stage_Proje.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult hakkimizda()
+        {
+            return View();
+        }
+        public IActionResult musteriaydinlatmametni()
+        {
+            return View();
+        }
+        public IActionResult sikcasorlansorular()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Stage-Proje/Stage-Proje/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Stage_Proje.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
